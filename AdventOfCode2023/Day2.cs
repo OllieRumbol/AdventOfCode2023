a3c2decdd418215d4db9c53cc963d03c79be300c
Oliver Bourne <otgb97@gmail.com>
2023-12-06 18:32:13 +0000

    day2

---
+++ AdventOfCode2023/Day2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    public int HighestRedCubeCount;
+
+    public int HighestBlueCubeCount;
+
+    public int HighestGreenCubeCount;
+
+    public string Print()
+    {
+        return $"Game {Id}: {HighestRedCubeCount} reds were used, {HighestBlueCubeCount} blues were used, {HighestGreenCubeCount} greens were used";
+    }
+
+    public Game(string game)
+    {
+        Id = int.Parse(game.Split(':').First().Split(" ").Last().ToString());
+
+        List<string> rounds = game.Split(':')[1].Split(';').ToList();
+        foreach (string round in rounds)
+        {
+            foreach (string cubes in round.Split(','))
+            {
+                int number = int.Parse(cubes.Trim().Split(' ').First());
+
+                if (cubes.Contains("red", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (number > HighestRedCubeCount)
+                    {
+                        HighestRedCubeCount = number;
+                    }
+                }
+                if (cubes.Contains("green", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (number > HighestGreenCubeCount)
+                    {
+                        HighestGreenCubeCount = number;
+                    }
+                }
+                if (cubes.Contains("blue", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (number > HighestBlueCubeCount)
+                    {
+                        HighestBlueCubeCount = number;
+                    }
+                }
+            }
+        }
+    }
+}
+
+public class Day2
+{
+    public string Part1()
+    {
+        List<string> inputs = File.ReadAllText(@"InputFiles\Day2.txt").Split('\n').ToList();
+
+        List<Game> results = new List<Game>();
+
+        foreach (string game in inputs)
+        {
+            results.Add(new Game(game));
+        }
+
+        List<Game> validGames = new List<Game>();
+
+        int maxNumberOfRedCubes = 12;
+        int maxNumberOfGreenCubes = 13;
+        int maxNumberOfBlueCubes = 14;
+
+
+        foreach (Game game in results)
+        {
+            if (game.HighestRedCubeCount <= maxNumberOfRedCubes && game.HighestGreenCubeCount <= maxNumberOfGreenCubes && game.HighestBlueCubeCount <= maxNumberOfBlueCubes)
+            {
+                validGames.Add(game);
+            }
+        }
+
+        return validGames.Sum(g => g.Id).ToString();
+    }
+
+    public string Part2()
+    {
+        int total = 0;
+        List<string> inputs = File.ReadAllText(@"InputFiles\Day2.txt").Split('\n').ToList();
+
+        List<Game> results = new List<Game>();
+
+        foreach (string game in inputs)
+        {
+            results.Add(new Game(game));
+        }
+
+
+
+        foreach (Game game in results)
+        {
+            total = total + (game.HighestRedCubeCount * game.HighestGreenCubeCount * game.HighestBlueCubeCount);
+        }
+
+        return total.ToString();
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -2,4 +2,8 @@
 
 Day1 day1 = new Day1();
 //Console.WriteLine(day1.Part1));
-Console.WriteLine(day1.Part2());
+//Console.WriteLine(day1.Part2());
+
+Day2 day2 = new Day2();
+//Console.WriteLine(day2.Part1());
+Console.WriteLine(day2.Part2());
