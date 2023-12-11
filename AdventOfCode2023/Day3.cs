c10aecb9cf184708c300f4c85adbc2ed963c8ab1
Oliver Bourne <otgb97@gmail.com>
2023-12-11 20:50:15 +0000

    day 3

---
+++ AdventOfCode2023/Day3.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023;
+
+public class Day3
+{
+    public string Part1()
+    {
+        List<string> lines = File.ReadLines(@"InputFiles\Day3.txt").ToList();
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < lines.Count; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                HashSet<int> numbersFound = new HashSet<int>();
+
+                if (lines[i][j] != '.' && char.IsNumber(lines[i][j]) == false)
+                {
+                    //Top left
+                    try
+                    {
+                        if (char.IsNumber(lines[i - 1][j - 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i - 1], j - 1));
+                        }
+                    }
+                    catch { }
+
+                    //Top
+                    try
+                    {
+                        if (char.IsNumber(lines[i - 1][j]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i - 1], j));
+                        }
+                    }
+                    catch { }
+
+                    //TopRight
+                    try
+                    {
+                        if (char.IsNumber(lines[i - 1][j + 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i - 1], j + 1));
+                        }
+                    }
+                    catch { }
+
+                    //left
+                    try
+                    {
+                        if (char.IsNumber(lines[i][j - 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i], j - 1));
+                        }
+                    }
+                    catch { }
+
+                    //Right
+                    try
+                    {
+                        if (char.IsNumber(lines[i][j + 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i], j + 1));
+                        }
+                    }
+                    catch { }
+
+                    //Bottom Left 
+                    try
+                    {
+                        if (char.IsNumber(lines[i + 1][j - 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i + 1], j - 1));
+                        }
+                    }
+                    catch { }
+
+                    //Bottom
+                    try
+                    {
+                        if (char.IsNumber(lines[i + 1][j]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i + 1], j));
+                        }
+                    }
+                    catch { }
+
+                    //BottomRight
+                    try
+                    {
+                        if (char.IsNumber(lines[i + 1][j + 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i + 1], j + 1));
+                        }
+                    }
+                    catch { }
+
+                    // Add to total
+                    foreach(int number  in numbersFound)
+                    {
+                        total = total + number;
+                    }
+
+                }
+            }
+        }
+
+        return total.ToString();
+    }
+
+    public string Part2()
+    {
+        List<string> lines = File.ReadLines(@"InputFiles\Day3.txt").ToList();
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < lines.Count; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                HashSet<int> numbersFound = new HashSet<int>();
+
+                if (lines[i][j] == '*')
+                {
+                    //Top left
+                    try
+                    {
+                        if (char.IsNumber(lines[i - 1][j - 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i - 1], j - 1));
+                        }
+                    }
+                    catch { }
+
+                    //Top
+                    try
+                    {
+                        if (char.IsNumber(lines[i - 1][j]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i - 1], j));
+                        }
+                    }
+                    catch { }
+
+                    //TopRight
+                    try
+                    {
+                        if (char.IsNumber(lines[i - 1][j + 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i - 1], j + 1));
+                        }
+                    }
+                    catch { }
+
+                    //left
+                    try
+                    {
+                        if (char.IsNumber(lines[i][j - 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i], j - 1));
+                        }
+                    }
+                    catch { }
+
+                    //Right
+                    try
+                    {
+                        if (char.IsNumber(lines[i][j + 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i], j + 1));
+                        }
+                    }
+                    catch { }
+
+                    //Bottom Left 
+                    try
+                    {
+                        if (char.IsNumber(lines[i + 1][j - 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i + 1], j - 1));
+                        }
+                    }
+                    catch { }
+
+                    //Bottom
+                    try
+                    {
+                        if (char.IsNumber(lines[i + 1][j]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i + 1], j));
+                        }
+                    }
+                    catch { }
+
+                    //BottomRight
+                    try
+                    {
+                        if (char.IsNumber(lines[i + 1][j + 1]))
+                        {
+                            numbersFound.Add(FindNumberInLine(lines[i + 1], j + 1));
+                        }
+                    }
+                    catch { }
+
+                    if(numbersFound.Count == 2)
+                    {
+                        int temp = 1;
+
+                        // Add to total
+                        foreach (int number in numbersFound)
+                        {
+                            temp = temp * number;
+                        }
+
+                        total = total + temp;
+                    }
+                }
+            }
+        }
+
+        return total.ToString();
+    }
+
+    private int FindNumberInLine(string line, int index)
+    {
+        string leftSide = "";
+        for (int i = index - 1; i >= 0; i--)
+        {
+            if (char.IsNumber(line[i]))
+            {
+                leftSide = $"{line[i]}{leftSide}";
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        string rightSide = "";
+        for (int i = index + 1; i < line.Length; i++)
+        {
+            if (char.IsNumber(line[i]))
+            {
+                rightSide = rightSide + line[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return int.Parse($"{leftSide}{line[index]}{rightSide}");
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,9 +1,13 @@
 ﻿using AdventOfCode2023;
 
-Day1 day1 = new Day1();
+//Day1 day1 = new Day1();
 //Console.WriteLine(day1.Part1));
 //Console.WriteLine(day1.Part2());
 
-Day2 day2 = new Day2();
+//Day2 day2 = new Day2();
 //Console.WriteLine(day2.Part1());
-Console.WriteLine(day2.Part2());
+//Console.WriteLine(day2.Part2());
+
+Day3 day3 = new Day3();
+//Console.WriteLine(day3.Part1());
+Console.WriteLine(day3.Part2());
